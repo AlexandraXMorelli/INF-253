8d6366951693a207a965912ee04f5e8781d8fcd9
AlexandraXMorelli <112849198+AlexandraXMorelli@users.noreply.github.com>
2022-12-14 23:22:37 -0600

    final

---
+++ finalscript.js
@@ -0,0 +1,5 @@
+function darkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+document.getElementById("page-title").style.fontSize = "60px";
+document.getElementById("page-title").style.textDecoration = "underline";
